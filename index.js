347912b630b024b4a38420d18f7deff8f2460bcd
lakshay sharma <lakshay0sharma@gmail.com>
2021-12-10 21:27:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const password = document.querySelector("#password");
+const eye = document.querySelector("#eye");
+
+eye.addEventListener("click", () => {
+  if(eye.classList.contains("fa-eye")){
+    password.setAttribute("type", "text");
+    eye.classList.replace("fa-eye", "fa-eye-slash");
+  }else {
+    password.setAttribute("type", "password");
+    eye.classList.replace("fa-eye-slash", "fa-eye");
+  }
+})
